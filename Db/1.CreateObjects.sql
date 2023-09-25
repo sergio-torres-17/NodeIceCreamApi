267286f6d2c84bbff685004f2be7f19210b2fb29
Sergio Saúl Torres Ibarra <50086305+sergio-torres-17@users.noreply.github.com>
2023-09-24 21:41:19 -0600

    Database Modifications

--- Db/1.CreateObjects.sql
+++ Db/1.CreateObjects.sql
@@ -1,8 +1,9 @@
-CREATE  DATABASE if not exists  icecreamstore;
+CREATE  DATABASE if not exists icecreamstore;
 use icecreamstore;
 CREATE TABLE flavors(
 flavor_id INT AUTO_INCREMENT,
 name TEXT,
+quantity_kg_stock float,
 date_inserted DATETIME DEFAULT NOW(),
 primary key(flavor_id)
 );
