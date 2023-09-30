b1bc0cf21099812d73a347c1cd1d9cae034acec5
Sergio Saúl Torres Ibarra <50086305+sergio-torres-17@users.noreply.github.com>
2023-09-30 01:38:28 -0600

    Modify Script API for docker

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@ import {createPool} from "mysql2/promise";
 
 
 export const pool  = createPool({
-    host: 'localhost',
+    host: '192.168.0.173',
     user: 'root',
     password: 'Cft6&/171200',
     port: 3306,
